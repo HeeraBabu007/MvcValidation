3b4fb486cb1505555c628b2b0769425904222dc4
Heera Babu <heerababu80@gmail.com>
2023-03-02 19:52:16 +0530

    update validation with pattern and number range like userid

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -1,12 +1,26 @@
 package com;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class User {
 
 	private String username;
 	@Size(min=1,message="Please enter password and its required")
+	@Pattern(regexp="^[a-zA-Z0-9]{3}",message="length must be 3")  
 	private String password;
+	
+    @Min(value=10, message="must be equal or greater than 10")  
+    @Max(value=25, message="must be equal or less than 25")  
+	private int userid;
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
 	public String getUsername() {
 		return username;
 	}
